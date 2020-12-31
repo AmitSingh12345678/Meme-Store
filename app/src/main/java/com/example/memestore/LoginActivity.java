ec327f3a79afe4b10c82fede66c16f3136ef77ea
Amit Singh <nandalamit1555@gmail.com>
2021-01-01 00:17:17 +0530

    Login Activity and Firebase Authentication and realTime database initialised

---
+++ app/src/main/java/com/example/memestore/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.memestore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private TextView forgotPassword;
+    private Button btnlogin;
+    private TextView signup;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        forgotPassword = findViewById(R.id.btnlogin);
+        btnlogin = findViewById(R.id.btnlogin);
+        signup = findViewById(R.id.textViewSignUp);
+
+        auth = FirebaseAuth.getInstance();
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                loginUser(email, password);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Invalid Credentials!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/memestore/RegisterActivity.java
@@ -0,0 +1,94 @@
+package com.example.memestore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
+    
+    private EditText inputUsername;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private EditText inputConfirmPassword;
+    private Button btnRegister;
+    private TextView alreadyHaveAnAccount;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        inputUsername=findViewById(R.id.inputUsername);
+        inputEmail=findViewById(R.id.inputEmail);
+        inputPassword=findViewById(R.id.inputPassword);
+        inputConfirmPassword=findViewById(R.id.inputConformPassword);
+        btnRegister=findViewById(R.id.btnRegister);
+        alreadyHaveAnAccount=findViewById(R.id.alreadyHaveAccount);
+
+        auth=FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=inputUsername.getText().toString();
+                String email=inputEmail.getText().toString();
+                String password=inputPassword.getText().toString();
+                String confirmPassword=inputConfirmPassword.getText().toString();
+                if(username.length()==0 || email.length()==0 || password.length()==0 || confirmPassword.length()==0){
+                    Toast.makeText(RegisterActivity.this, "Empty Credential", Toast.LENGTH_SHORT).show();
+                }else if(!password.equals(confirmPassword)){
+                    Toast.makeText(RegisterActivity.this, "Enter same passwords", Toast.LENGTH_SHORT).show();
+                }else{
+                    registerUser(username,email,password);
+                }
+            }
+        });
+        alreadyHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+    }
+
+    private void registerUser(final String username, String email, String password) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    FirebaseDatabase.getInstance().getReference("Users").child(auth.getCurrentUser().getUid()).setValue(username).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful())
+                            Toast.makeText(RegisterActivity.this, "username also updated", Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(RegisterActivity.this, "Failed!!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+                    finish();
+                }else{
+                    Log.d(TAG, "onComplete: Failed!!");
+                }
+            }
+        });
+
+    }
+}
